bbfc0295fa916cf2797be2f8c3e796d69eca927f
liluchang <recan.li@redteamobile.com>
2019-08-09 14:13:54 +0800

    feat: Delete agent/monitor
    feat: Delete agent/monitor

--- agent/lpa/lpad/inc/ldsd.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LDSD_H__
-#define __LDSD_H__
-
-#include <stdint.h>
-
-int get_euicc_configured_address(uint8_t *addr, uint16_t *size);
-int set_default_dp_address(const char *addr, uint8_t *out, uint16_t *out_size);
-
-#endif  // __LDSD_H__
