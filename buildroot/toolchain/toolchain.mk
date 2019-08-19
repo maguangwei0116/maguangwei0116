2a43137a249bfc76a289f8f983bdddb31fb661d7
liluchang <recan.li@redteamobile.com>
2019-08-19 10:32:54 +0800

    delete: Delete some target in toolchain.mk

--- buildroot/toolchain/toolchain.mk
+++ buildroot/toolchain/toolchain.mk
@@ -1,5 +1,4 @@
 
-
 .PHONY: toolchain toolchain-clean
 
 TOOLCHAIN_PATH=$(call qstrip,$(BR2_TOOLCHAIN_INSTALL_PATH))
@@ -9,15 +8,4 @@ TARGET_SYSROOT=$(call qstrip,$(BR2_TOOLCHAIN_SYSROOT))
 TARGET_SYSINC="$(call qstrip,$(BR2_TOOLCHAIN_SYSINC))"
 TARGET_LIB_PATH=$(TOOLCHAIN_PATH)/$(dir $(call qstrip,$(BR2_TOOLCHAIN_PREFIX)))/../lib
 
-toolchain:
-	@echo "toolchain building $(TOOLCHAIN_PATH)"
-	test -e $(TOOLCHAIN_PATH)/ndk || \
-	(wget --output-document=$(DL_DIR)/toolchain-cross.bin $(BR2_TOOLCHAIN_URL) && \
-	mkdir -p $(TOOLCHAIN_PATH) && echo "Install toolchain ..." && \
-	$(TAR) $(TAR_STRIP_COMPONENTS)=0 -C $(TOOLCHAIN_PATH) $(TAR_OPTIONS) $(DL_DIR)/toolchain-cross.bin)
-	rm -rf $(DL_DIR)/toolchain-cross.bin
-
-toolchain-clean:
-	@echo "toolchain-clean"
-	rm -rf $(TOOLCHAIN_PATH)
 
