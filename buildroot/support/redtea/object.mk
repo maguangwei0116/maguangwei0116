7da8f97f4953a157e846741b03b277887aabf9c0
liluchang <recan.li@redteamobile.com>
2019-08-23 17:43:11 +0800

    feat: Add pre-compile in object.mk

--- buildroot/support/redtea/object.mk
+++ buildroot/support/redtea/object.mk
@@ -6,6 +6,7 @@ DEPS			= $(foreach dep,$(OBJS),$(dir $(dep)).$(notdir $(dep)).d)
 $(O)/%.o : %.c
 	@[ -d $(dir $@) ] || $(MKDIR) -p $(dir $@)
 #	$($(quiet)do_cc) -c $(CFLAGS) -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	$($(quiet)do_cc) -E $(CFLAGS) -o "$(@:%.o=%.i)" "$<"
 	$($(quiet)do_cc) -c $(CFLAGS) -fPIC -MD -MF "$(dir $@).$(notdir $@).d" -o"$@" "$<"
 
 $(O)/%.o : %.s
