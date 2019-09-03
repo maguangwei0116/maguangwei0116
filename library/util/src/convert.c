e1f72dde770fd53706a57b1601a324c66cff89ab
liluchang <recan.li@redteamobile.com>
2019-09-03 17:40:07 +0800

    feat: Update convert.c

--- library/util/src/convert.c
+++ library/util/src/convert.c
@@ -94,3 +94,48 @@ int bytes2hexstring(const uint8_t *bytes, uint16_t length, char *hextring)
 
     return RT_SUCCESS;
 }
+
+rt_bool strncpy_case_insensitive(int8_t *src,int8_t *obj,int16_t len)
+{
+    int i;
+    for (i = 0; i < len; i++) {
+
+        if (src[i] == obj[i] || src[i] == (obj[i] + 'a' - 'A') || obj[i] == src[i] + 'a' - 'A') {
+            continue;
+        } else {
+            MSG_PRINTF(LOG_WARN, "no.%d number is not equal,src:%c,obj:%c\n", i, src[i], obj[i]);
+            return RT_FALSE;
+        }
+    }
+    return RT_TRUE;
+}
+
+static unsigned char to_ascii(unsigned char ch)
+{
+    if (ch <= 9) {
+        ch += ('0' - 0);
+    }
+    else if (ch >= 0xa && ch <= 0xf) {
+        ch += ('A' - 0xa);
+    } else {
+        ch = 'F';
+    }
+    return ch;
+}
+
+void bytestring_to_charstring(int8_t *bytestring,int8_t *charstring,int16_t length)
+{
+    int32_t i = 0;
+    int8_t left,right;
+    for (i = 0; i < length ; i ++) {
+        left = to_ascii((bytestring[i] >> 4) & 0x0F);
+        right = to_ascii(bytestring[i] & 0x0F);
+
+        charstring[2 * i] = left;
+        charstring[2 * i + 1] = right;
+    }
+    charstring[2*i] = '\0';
+}
+
+
+
