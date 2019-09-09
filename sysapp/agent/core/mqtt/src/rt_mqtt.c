320e09f7581a7ad79fc4d20e7a55f54baa581222
liluchang <recan.li@redteamobile.com>
2019-09-09 10:19:46 +0800

    feat: Add upload registered event after connect mqtt ok

--- sysapp/agent/core/mqtt/src/rt_mqtt.c
+++ sysapp/agent/core/mqtt/src/rt_mqtt.c
@@ -523,6 +523,8 @@ static rt_bool rt_mqtt_connect_server(mqtt_param_t *param)
     opts->last_connect_status = MQTT_CONNECT_SUCCESS;
     opts->try_connect_timer = 0;
     param->alias_rc = 1;
+
+    upload_event_report("REGISTERED", NULL, 0, NULL);
     
     return RT_TRUE;
 }
