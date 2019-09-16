15dd6ee62b88c04c522aa58ffa89f383466f7f3c
liluchang <recan.li@redteamobile.com>
2019-09-16 11:19:40 +0800

    feat: Add downstream-reboot/reset handle

---
+++ sysapp/agent/core/downstream/src/downstream_reset.c
@@ -0,0 +1,62 @@
+
+#include "downstream.h"
+#include "rt_type.h"
+#include "rt_os.h"
+#include "log.h"
+
+#include "cJSON.h"
+
+static int32_t downstream_reset_parser(const void *in, char *tranId, void **out)
+{
+    int32_t ret;
+    cJSON *msg = NULL;
+    cJSON *tran_id = NULL;
+
+    msg =  cJSON_Parse((const char *)in);
+    if (!msg) {
+        MSG_PRINTF(LOG_WARN, "msg error\n");
+        ret = -1;
+        goto exit_entry;
+    }
+
+    tran_id = cJSON_GetObjectItem(msg, "tranId");
+    if (!tran_id) {
+        MSG_PRINTF(LOG_WARN, "tran_id error\n");
+        ret = -2;
+        goto exit_entry;
+    }
+    
+    rt_os_strcpy(tranId, tran_id->valuestring);
+    MSG_PRINTF(LOG_WARN, "tranId: %s, %p, stelen=%d\n", tranId, tranId, rt_os_strlen(tran_id->valuestring));
+
+    ret = 0;
+
+exit_entry:
+
+    if (msg != NULL) {
+        cJSON_Delete(msg);
+        msg = NULL;
+    }
+
+    return ret;
+}
+
+static int32_t downstream_reset_handler(const void *in, const char *event, void **out)
+{
+    int32_t ret = 0;
+
+    (void)in;
+    (void)event;
+    *out = NULL;
+    
+    MSG_PRINTF(LOG_WARN, "wait 3 seconds to reset terminal ...\n");
+    rt_os_sleep(3);
+    rt_os_reboot();  // reset terminal
+
+exit_entry:
+
+    return ret;
+}
+
+DOWNSTREAM_METHOD_OBJ_INIT(RESET, MSG_ID_IDLE, NULL, downstream_reset_parser, downstream_reset_handler);
+
