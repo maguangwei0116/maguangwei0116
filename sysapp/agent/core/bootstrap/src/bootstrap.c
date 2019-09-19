2f53f7c0acfe3c852aa8cdc49b369114297d8991
fangshanxing <shanxing.fang@redteamobile.com>
2019-09-19 14:20:26 +0800

    fix:Share profile hash check failure handling

--- sysapp/agent/core/bootstrap/src/bootstrap.c
+++ sysapp/agent/core/bootstrap/src/bootstrap.c
@@ -25,6 +25,7 @@
 static uint32_t g_single_interval_time = 10; // 激活失败单次间隔时间
 static uint32_t g_max_retry_times = 7; // 最大重试次数
 static uint32_t g_sleep_time = 24 * 60 * 60; // 休眠时长
+static uint32_t g_is_profile_damaged = RT_ERROR;
 
 static uint32_t get_random(void)
 {
@@ -49,13 +50,18 @@ static void bootstrap_select_profile(void)
 
 int32_t init_bootstrap(void *arg)
 {
-    return init_profile_file(NULL);
+    g_is_profile_damaged = init_profile_file(NULL);
+    return g_is_profile_damaged;
 }
 
 void bootstrap_event(const uint8_t *buf, int32_t len, int32_t mode)
 {
+    if (g_is_profile_damaged != RT_SUCCESS){
+        MSG_PRINTF(LOG_INFO, "The share profile is damaged.\n");
+        return g_is_profile_damaged;
+    }
     MSG_PRINTF(LOG_INFO, "The current mode is %d.\n", mode);
-    if (mode == 0) {
+    if (mode == MSG_BOOTSTRAP_SELECT_CARD) {
         bootstrap_select_profile();
     } else if (mode == MSG_BOOTSTRAP_DISCONNECTED) {
         MSG_PRINTF(LOG_INFO, "g_single_interval_time:%d\n", g_single_interval_time);
