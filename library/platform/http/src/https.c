7f5d887a33ac004f67df883c812037f1330fae00
liluchang <recan.li@redteamobile.com>
2019-09-20 19:18:50 +0800

    fix: Fix memory leak when get peer cert subject name

--- library/platform/http/src/https.c
+++ library/platform/http/src/https.c
@@ -237,11 +237,12 @@ int https_init(https_ctx_t *https_ctx, const char *host, const char *port, const
     }
 
     X509* cert = SSL_get_peer_certificate(https_ctx->ssl);
-    MSG_PRINTF(LOG_INFO, "cert: %s\n", cert);
     if (cert) {  // Free immediately
         char *line;
+        //MSG_PRINTF(LOG_INFO, "cert: %s\n", cert);
         line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
         MSG_PRINTF(LOG_INFO, "Subject Name: %s\n", line);
+        rt_os_free(line);
         line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
         MSG_PRINTF(LOG_INFO, "Issuer: %s\n", line);
         res = SSL_get_verify_result(https_ctx->ssl);
