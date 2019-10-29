40490fb31b884d75cdf128d9a8ea83f51d15b596
steve.hao@redteamobile.com <steve.hao@redteamobile.com>
2019-10-29 15:23:25 +0800

    feat:Add script for make crypto tool

---
+++ sysapp/monitor/core/download/download_file.h
@@ -0,0 +1,19 @@
+
+/*******************************************************************************
+ * Copyright (c) redtea mobile.
+ * File name   : download_file.h
+ * Date        : 2019.10.29
+ * Note        :
+ * Description :
+ * Contributors: RT - create the file
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Sublime text
+ *******************************************************************************/
+
+#ifndef __DOWNLOAD_FILE_H__
+#define __DOWNLOAD_FILE_H__
+
+#include "rt_type.h"
+
+#endif  // __DOWNLOAD_FILE_H__
---
+++ sysapp/monitor/core/download/src/download_file.c
@@ -0,0 +1,14 @@
+
+/*******************************************************************************
+ * Copyright (c) redtea mobile.
+ * File name   : download_file.c
+ * Date        : 2019.10.29
+ * Note        :
+ * Description :
+ * Contributors: RT - create the file
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Sublime text
+ *******************************************************************************/
+
+#include "download_file.h"
