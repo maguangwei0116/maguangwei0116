4feac96bd5091dae604c8cf895ccb77bd82fd36b
liluchang <recan.li@redteamobile.com>
2019-11-13 13:31:03 +0800

    fix: Update support/scripts/sign_file.sh, add check inpur param number

--- buildroot/support/scripts/sign_file.sh
+++ buildroot/support/scripts/sign_file.sh
@@ -1,13 +1,21 @@
 #! /bin/bash
 
+if [ $# -lt 2 ] ; then
+echo "Error param input ($#) !"
+echo "Usage: $0 file_name file_path"
+echo "file_name: The file name in FS"
+echo "file_path: The file path in FS."
+exit
+fi
+
 file=$1     #file name
 path=$2     #script path
 
 #check compile quite or not
 if [ "$Q" == "@" ];then
-quit=1
+quiet=1
 else
-quit=0
+quiet=0
 fi
 
 name_block_size=20        #max file name len
@@ -16,13 +24,13 @@ name=`echo ${file##*/}${file_name_block}`
 echo -n ${name:0:${name_block_size}} >> ${file}
 
 sk="76931FAC9DAB2B36C248B87D6AE33F9A62D7183A5D5789E4B2D6B441E2411DC7"     # ecc sk
-if [ "$quit" == "0" ];then
+if [ "$quiet" == "0" ];then
 echo "sk "${sk}
 fi
 
 hash_out=$(sha256sum ${file})     #hash
 hash=${hash_out%%" "*}
-if [ "$quit" == "0" ];then
+if [ "$quiet" == "0" ];then
 echo "hash "${hash}
 fi
 
@@ -30,7 +38,7 @@ signature_out=$(${path}/crypto -h ${hash} -s ${sk})
 #echo ${signature_out}
 
 signature=${signature_out##*"signature:"}
-if [ "$quit" == "0" ];then
+if [ "$quiet" == "0" ];then
 echo "signature "${signature}    # len = 129
 fi
 
