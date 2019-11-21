1f96e66b7737a822863f021f0396d64c6b7ebda9
liluchang <recan.li@redteamobile.com>
2019-11-21 14:25:57 +0800

    fix: Adjust network detection module to fix android implemet

--- sysapp/agent/core/networkd/network_detection.h
+++ sysapp/agent/core/networkd/network_detection.h
@@ -16,8 +16,41 @@
 
 #include "rt_type.h"
 
+#ifdef CFG_PLATFORM_9X07
+
+#include "dial_up.h"
+
+// these values defined in dial_up.h
+
+#define RT_DSI_STATE_CALL_IDLE              DSI_STATE_CALL_IDLE
+#define RT_DSI_STATE_CALL_CONNECTING        DSI_STATE_CALL_CONNECTING
+#define RT_DSI_STATE_CALL_CONNECTED         DSI_STATE_CALL_CONNECTED
+#define RT_DSI_STATE_CALL_DISCONNECTING     DSI_STATE_CALL_DISCONNECTING
+#define RT_DSI_STATE_CALL_MAX               DSI_STATE_CALL_MAX
+
+#endif
+
+#ifdef CFG_PLATFORM_ANDROID
+
+typedef enum DSI_STATE_CALL_STATE {
+    RT_DSI_STATE_CALL_IDLE = 0,             /* network idle (disconnected)  */
+    RT_DSI_STATE_CALL_CONNECTING,           /* network connecting           */
+    RT_DSI_STATE_CALL_CONNECTED,            /* network connected            */
+    RT_DSI_STATE_CALL_DISCONNECTING,        /* network disconnecting        */
+    RT_DSI_STATE_CALL_MAX                   /* network unknow state         */
+} dsi_state_call_state_e;
+
+#endif
+
 int32_t init_network_detection(void *arg);
 int32_t network_detect_event(const uint8_t *buf, int32_t len, int32_t mode);
+
+/**
+ brief:     set current network state
+ param[in]: state @ref dsi_state_call_state_e
+ note:      agent only pay close attention to 2 state: [RT_DSI_STATE_CALL_CONNECTED and RT_DSI_STATE_CALL_IDLE]
+ */
+void    network_update_state(int32_t state); 
 
 #endif // __NETWORK_DETECTION_H__
 
--- sysapp/agent/core/networkd/src/network_detection.c
+++ sysapp/agent/core/networkd/src/network_detection.c
@@ -10,28 +10,75 @@
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Sublime text
  *******************************************************************************/
+
+#include "network_detection.h"
+#include "downstream.h"
+
+void network_update_state(int32_t state)
+{
+    #define NETWORK_STATE_NOT_READY     -1
+    
+    static int32_t g_network_state      = NETWORK_STATE_NOT_READY;
+    
+    if (state == g_network_state) {
+        return;
+    }
+
+    g_network_state = state;
+
+    if (g_network_state == RT_DSI_STATE_CALL_CONNECTED) {  // network connected
+        msg_send_agent_queue(MSG_ID_BROAD_CAST_NETWORK, MSG_NETWORK_CONNECTED, NULL, 0);
+        card_detection_disable();
+    } else if (g_network_state == RT_DSI_STATE_CALL_IDLE) {  // network disconnected
+        msg_send_agent_queue(MSG_ID_BROAD_CAST_NETWORK, MSG_NETWORK_DISCONNECTED, NULL, 0);
+        card_detection_enable();
+        g_network_state = NETWORK_STATE_NOT_READY;
+    }
+
+    #undef NETWORK_STATE_NOT_READY
+}
+
+int32_t network_detect_event(const uint8_t *buf, int32_t len, int32_t mode)
+{
+    int32_t status = 0;
+    int32_t ret = RT_ERROR;
+    downstream_msg_t *downstream_msg = (downstream_msg_t *)buf;
+
+    (void)mode;
+    //MSG_PRINTF(LOG_INFO, "msg: %s ==> method: %s ==> event: %s\n", downstream_msg->msg, downstream_msg->method, downstream_msg->event);
+
+    ret = downstream_msg->parser(downstream_msg->msg, downstream_msg->tranId, &downstream_msg->private_arg);
+    if (downstream_msg->msg) {
+        rt_os_free(downstream_msg->msg);
+        downstream_msg->msg = NULL;
+    }
+    if (ret == RT_ERROR) {
+        return ret;
+    }
+
+    // MSG_PRINTF(LOG_WARN, "tranId: %s, %p\n", downstream_msg->tranId, downstream_msg->tranId);
+    status = downstream_msg->handler(downstream_msg->private_arg,  downstream_msg->event, &downstream_msg->out_arg);
+    upload_event_report(downstream_msg->event, (const char *)downstream_msg->tranId, status, downstream_msg->out_arg);
+
+    return RT_SUCCESS;
+}
+
 #ifdef CFG_PLATFORM_9X07
 
-#include "dial_up.h"
-#include "rt_timer.h"
-#include "downstream.h"
 #include "card_manager.h"
 #include "card_detection.h"
 #include "bootstrap.h"
-#include "rt_timer.h"
 
 #define MAX_INIT_RETRY_CNT              3
 #define DELAY_100MS                     100
 #define MAX_WAIT_BOOTSTRAP_TIME         150  // 150 * 100ms = 15000ms = 15S
-#define NETWORK_STATE_NOT_READY         -1
 
 typedef struct TASK_PARAM {
     int32_t *           profile_damaged;
     profile_type_e *    type;
 } task_param_t;
 
-static int32_t g_network_state          = NETWORK_STATE_NOT_READY;
-static int32_t g_network_new_state      = 0;
+static task_param_t     g_task_param;
 
 /*
 avoid this case:
@@ -95,39 +142,15 @@ exit_entry:
     rt_exit_task(NULL);
 }
 
-static void network_state(int32_t state)
-{
-    if (state == g_network_state) {
-        return;
-    }
-
-    g_network_state = state;
-
-    if (g_network_state == DSI_STATE_CALL_CONNECTED) {  // network connected
-        msg_send_agent_queue(MSG_ID_BROAD_CAST_NETWORK, MSG_NETWORK_CONNECTED, NULL, 0);
-        card_detection_disable();
-    } else if (g_network_state == DSI_STATE_CALL_IDLE) {  // network disconnected
-        msg_send_agent_queue(MSG_ID_BROAD_CAST_NETWORK, MSG_NETWORK_DISCONNECTED, NULL, 0);
-        card_detection_enable();
-        g_network_state = NETWORK_STATE_NOT_READY;
-    }
-}
-
-static task_param_t g_task_param;
-
 int32_t init_network_detection(void *arg)
 {
     rt_task task_id = 0;
     int32_t ret = RT_ERROR;    
-    profile_type_e *type;
-    int32_t *profile_damaged;
 
-    type                            = &(((public_value_list_t *)arg)->card_info->type);
-    profile_damaged                 = ((public_value_list_t *)arg)->profile_damaged;
-    g_task_param.type               = type;
-    g_task_param.profile_damaged    = profile_damaged;
+    g_task_param.type               = &(((public_value_list_t *)arg)->card_info->type);
+    g_task_param.profile_damaged    = ((public_value_list_t *)arg)->profile_damaged;
 
-    dial_up_set_dial_callback((void *)network_state);
+    dial_up_set_dial_callback((void *)network_update_state);
     
     ret = rt_create_task(&task_id, (void *)network_detection_task, &g_task_param);
     if (ret != RT_SUCCESS) {
@@ -140,34 +163,7 @@ int32_t init_network_detection(void *arg)
 
 #endif
 
-int32_t network_detect_event(const uint8_t *buf, int32_t len, int32_t mode)
-{
-    int32_t status = 0;
-    int32_t ret = RT_ERROR;
-    downstream_msg_t *downstream_msg = (downstream_msg_t *)buf;
-
-    (void)mode;
-    //MSG_PRINTF(LOG_INFO, "msg: %s ==> method: %s ==> event: %s\n", downstream_msg->msg, downstream_msg->method, downstream_msg->event);
-
-    ret = downstream_msg->parser(downstream_msg->msg, downstream_msg->tranId, &downstream_msg->private_arg);
-    if (downstream_msg->msg) {
-        rt_os_free(downstream_msg->msg);
-        downstream_msg->msg = NULL;
-    }
-    if (ret == RT_ERROR) {
-        return ret;
-    }
-
-    // MSG_PRINTF(LOG_WARN, "tranId: %s, %p\n", downstream_msg->tranId, downstream_msg->tranId);
-    status = downstream_msg->handler(downstream_msg->private_arg,  downstream_msg->event, &downstream_msg->out_arg);
-    upload_event_report(downstream_msg->event, (const char *)downstream_msg->tranId, status, downstream_msg->out_arg);
-
-    return RT_SUCCESS;
-}
-
 #ifdef CFG_PLATFORM_ANDROID
-
-#include "network_detection.h"
 
 int32_t init_network_detection(void *arg)
 {
