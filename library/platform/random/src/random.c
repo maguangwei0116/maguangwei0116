de0c4cdb0cb4efe2b7ce638a3d8664111ed1898b
liluchang <recan.li@redteamobile.com>
2019-11-28 10:57:35 +0800

    fix: Fix rt_get_random_num implement

--- library/platform/random/src/random.c
+++ library/platform/random/src/random.c
@@ -1,14 +1,20 @@
 
 #include "rt_type.h"
-#include "rt_manage_data.h"
+#include "file.h"
 
-#define URANDOM_DEV             "/dev/urandom"
+#define URANDOM_DEV     "/dev/urandom"
 
 uint64_t rt_get_random_num(void)
 {
     uint64_t num;
-
-    rt_read_data(URANDOM_DEV, 0, (uint8_t *)&num, sizeof(num));
+    rt_fshandle_t fp = NULL;
+    
+    fp = linux_fopen(URANDOM_DEV, "r");
+    if (fp) {
+        linux_fseek(fp, 0, RT_FS_SEEK_SET);
+        linux_fread((uint8_t *)&num, 1, sizeof(num), fp);        
+        linux_fclose(fp);
+    }
 
     return num;
 }
