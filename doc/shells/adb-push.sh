a8da9c3880471d6895ef593b80fb94c25fbff7df
liluchang <recan.li@redteamobile.com>
2019-12-09 12:19:36 +0800

    feat: Add autoly packet released ubi files

--- doc/shells/adb-push.sh
+++ doc/shells/adb-push.sh
@@ -2,23 +2,23 @@
 
 #!/bin/bash -e
 
-adb push ./targets/*agent* /usr/bin/rt_agent
+adb push ./app_targets/*agent* /usr/bin/rt_agent
 adb shell chmod +x /usr/bin/rt_agent
 
-adb push ./targets/*monitor* /usr/bin/rt_monitor
+adb push ./app_targets/*monitor* /usr/bin/rt_monitor
 adb shell chmod +x /usr/bin/rt_monitor
 
-adb push ./targets/*libcomm.so* /usr/lib/libcomm.so
+adb push ./app_targets/*libcomm.so* /usr/lib/libcomm.so
 adb shell chmod +x /usr/lib/libcomm.so
 
-adb push ./shells/start_redtea_app /etc/init.d/start_redtea_app
+adb push ./app_shells/start_redtea_app /etc/init.d/start_redtea_app
 adb shell chmod +x /etc/init.d/start_redtea_app
 
-adb push ./shells/start_redtea_keep /etc/init.d/start_redtea_keep
+adb push ./app_shells/start_redtea_keep /etc/init.d/start_redtea_keep
 adb shell chmod +x /etc/init.d/start_redtea_keep
 
-if [ -e ./targets/test_lpa ]; then
-adb push ./targets/test_lpa /data/redtea/test_lpa
+if [ -e ./app_targets/test_lpa ]; then
+adb push ./app_targets/test_lpa /data/redtea/test_lpa
 adb shell chmod +x /data/redtea/test_lpa
 fi
 
