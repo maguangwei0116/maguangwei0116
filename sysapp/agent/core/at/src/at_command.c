9b189eec3d623298cc2537e5b6ab72042626c064
liluchang <recan.li@redteamobile.com>
2019-12-26 17:29:21 +0800

    fix: Add open/standard module control for customer at

--- sysapp/agent/core/at/src/at_command.c
+++ sysapp/agent/core/at/src/at_command.c
@@ -10,53 +10,50 @@
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Sublime text 2
  *******************************************************************************/
+
+#include "rt_type.h"
+ 
+#ifdef CFG_STANDARD_MODULE
+
 #include "agent_queue.h"
 #include "at_command.h"
-#ifdef CFG_STANDARD_MODULE
 #include "customer_at.h"
-#endif
 #include "ubi.h"
 #include "agent2monitor.h"
 #include "lpa.h"
 #include "log.h"
 
-#define       AT_TYPE_GET_INFO               '0'
-#define       AT_TYPE_CONFIG_UICC            '1'
+#define AT_TYPE_GET_INFO                '0'
+#define AT_TYPE_CONFIG_UICC             '1'
 
-#define       AT_GET_EID                     '0'
-#define       AT_GET_ICCIDS                  '1'
-#define       AT_GET_UICC_TYPE               '2'
+#define AT_GET_EID                      '0'
+#define AT_GET_ICCIDS                   '1'
+#define AT_GET_UICC_TYPE                '2'
 
-#define       AT_SWITCH_TO_PROVISIONING      '0'
-#define       AT_SWITCH_TO_OPERATION         '1'
-#define       AT_CONFIG_LPA_CHANNEL          '2'
-#define       AT_UPGRADE_UBI_FILE            '3'
+#define AT_SWITCH_TO_PROVISIONING       '0'
+#define AT_SWITCH_TO_OPERATION          '1'
+#define AT_CONFIG_LPA_CHANNEL           '2'
+#define AT_UPGRADE_UBI_FILE             '3'
 
-#define       AT_CONTENT_DELIMITER           ','
+#define AT_CONTENT_DELIMITER            ','
 
-#define       AT_CFG_VUICC                   "\"vUICC\""
-#define       AT_CFG_EUICC                   "\"eUICC\""
-#define       AT_CFG_UICC_LEN                7 // 5+2
+#define AT_CFG_VUICC                    "\"vUICC\""
+#define AT_CFG_EUICC                    "\"eUICC\""
+#define AT_CFG_UICC_LEN                 7 // 5+2
 
-#define       OTA_UPGRADE_OEMAPP_UBI         "oemapp.ubi"
-#ifdef CFG_STANDARD_MODULE
-#define       OTA_UPGRADE_USR_AGENT          "/usrdata/redtea/rt_agent"
-#endif
+#define OTA_UPGRADE_OEMAPP_UBI          "oemapp.ubi"
+#define OTA_UPGRADE_USR_AGENT           "/usrdata/redtea/rt_agent"
 
-#ifdef CFG_STANDARD_MODULE
 /*
 handle function name: xxx_at_cmd_handle
 */
-#define       AT_CMD_DEF(label)              static at_cmd_t g_at_cmd_##label = {#label, label##_at_cmd_handle}
-#define       AT_CMD_INIT(label)             init_customer_at(&g_at_cmd_##label)
-#define       AT_CMD_INSTALL(label)          \
-              {\
-                  AT_CMD_DEF(label);\
-                  AT_CMD_INIT(label);\
-              }
-#else
-#define       AT_CMD_INSTALL(label)          do {} while(0)
-#endif
+#define AT_CMD_DEF(label)               static at_cmd_t g_at_cmd_##label = {#label, label##_at_cmd_handle}
+#define AT_CMD_INIT(label)              init_customer_at(&g_at_cmd_##label)
+#define AT_CMD_INSTALL(label)           \
+        {\
+          AT_CMD_DEF(label);\
+          AT_CMD_INIT(label);\
+        }
 
 static public_value_list_t *g_p_value_list  = NULL;
 
@@ -187,9 +184,7 @@ static int32_t uicc_at_cmd_handle(const char *cmd, char *rsp, int32_t len)
                 if (rt_os_strlen(ubi_abs_file)) {
                     ret = ipc_file_verify_by_monitor(ubi_abs_file, real_file_name);
                     if (!ret && !rt_os_strcmp(OTA_UPGRADE_OEMAPP_UBI, real_file_name)) {
-#ifdef CFG_STANDARD_MODULE
                         linux_delete_file(OTA_UPGRADE_USR_AGENT);  // must delete agent to create a new one !!!
-#endif
                         ret = ubi_update((const char *)ubi_abs_file);
                         if (ret == RT_SUCCESS) {
                             /* rsp: ,para,"ubi_file" */
@@ -207,7 +202,6 @@ static int32_t uicc_at_cmd_handle(const char *cmd, char *rsp, int32_t len)
     return ret;
 }
 
-
 int32_t init_at_command(void *arg)
 {
     g_p_value_list = ((public_value_list_t *)arg);
@@ -217,3 +211,15 @@ int32_t init_at_command(void *arg)
     
     return RT_SUCCESS;
 }
+
+#else
+
+int32_t init_at_command(void *arg)
+{
+    (void)arg;
+    
+    return RT_SUCCESS;
+}
+
+#endif
+
