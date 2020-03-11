6694e5dd5d263b17013f49dfe44c4a6bca6d286e
steve.hao@redteamobile.com <steve.hao@redteamobile.com>
2020-03-11 12:02:44 +0800

    fix:Ecc verify success

--- sysapp/monitor/core/inspect/src/inspect_file.c
+++ sysapp/monitor/core/inspect/src/inspect_file.c
@@ -24,16 +24,14 @@
 #define RT_CHECK_ERR(process, result)   if((process) == result){ MSG_PRINTF(LOG_WARN, "[%s] error\n", #process);  goto end;}
 
 // static const uint8_t *g_verify_pk = "B37F3BAD94DFCC1FBDB0FBF608802FA72D38FAEE3AB8CBBF63BF6C99DA9E31FAE1465F1BCFCAF85A6626B938D1BD12D6901833047C50FE8ED67B84CFCFECCFEA";
-static const uint8_t *g_verify_pk = "00B7EE6A549EB9F3FA3BA337AAC91FC35A135E2E606CA623C9FF4C85A7552FB499987E665D0A3D3108CFB46297F5D90827D34C1F47F6F05E10376AE16C3EED29";
+static const uint8_t *g_verify_pk = "0000B7EE6A549EB9F3FA3BA337AAC91FC35A135E2E606CA623C9FF4C85A7552FB499987E665D0A3D3108CFB46297F5D90827D34C1F47F6F05E10376AE16C3EED29";
 /**
  * function: verify a section data by input public key
- * return value: 
+ * return value:
  * - 0 (flase) : verify fail
  * - 1 (true)  : verify ok
  * notes: This function was defined in libvuicc.a
  */
-
-
 extern int ecc_verify(uint8_t *input, int input_len, const uint8_t *pubkey, int pubkey_len, uint8_t *sigBuff, int  sig_len);
 // extern int ecc_hash_verify_signature(uint8_t *input, int input_len, const uint8_t *pubkey, int pubkey_len, uint8_t *sigBuff, int sig_len);
 
@@ -42,7 +40,7 @@ rt_bool monitor_get_file_sign_data(const char *file_name, uint8_t *sign, int32_t
     rt_fshandle_t fp = NULL;
     int8_t sign_buffer[PRIVATE_HASH_STR_LEN + 1] = {0};
     rt_bool ret = RT_FALSE;
-    
+
     RT_CHECK_ERR(fp = linux_fopen((char *)file_name, "r"), NULL);
     RT_CHECK_ERR(linux_fseek(fp, -(PRIVATE_HASH_STR_LEN), RT_FS_SEEK_END), -1);
     RT_CHECK_ERR(linux_fread(sign_buffer, PRIVATE_HASH_STR_LEN, 1, fp), 0);
@@ -50,7 +48,7 @@ rt_bool monitor_get_file_sign_data(const char *file_name, uint8_t *sign, int32_t
     if (sign) {
         rt_os_memcpy(sign, sign_buffer, PRIVATE_HASH_STR_LEN);
     }
-    
+
     if (len) {
         *len = PRIVATE_HASH_STR_LEN;
     }
@@ -125,17 +123,17 @@ rt_bool monitor_inspect_file(const char *file_name, const char *exp_real_file_na
 
     RT_CHECK_ERR(linux_fread(sign_buffer, PRIVATE_HASH_STR_LEN, 1, fp), 0);
     MSG_PRINTF(LOG_INFO, "signature_buffer:%s\n", sign_buffer);
-    
-    linux_fseek(fp, -(PRIVATE_HASH_STR_LEN + MAX_FILE_INFO_LEN), RT_FS_SEEK_END); 
+
+    linux_fseek(fp, -(PRIVATE_HASH_STR_LEN + MAX_FILE_INFO_LEN), RT_FS_SEEK_END);
     RT_CHECK_ERR(linux_fread(file_info, MAX_FILE_INFO_LEN, 1, fp), 0);
     MSG_PRINTF(LOG_INFO, "file_info:%s\n", file_info);
     sha256_final(&sha_ctx, (uint8_t *)hash_out);
 
-    // bytestring_to_charstring((const char *)hash_out, (char *)hash_result, MAX_FILE_HASH_BYTE_LEN);
+    bytestring_to_charstring((const char *)hash_out, (char *)hash_result, MAX_FILE_HASH_BYTE_LEN);
 
     hexstring2bytes(sign_buffer, sign_buffer_out, &sign_buffer_out_len);
 
-    if (inspect_abstract_content(hash_out, sign_buffer_out) != RT_TRUE) {
+    if (inspect_abstract_content(hash_result, sign_buffer_out) != RT_TRUE) {
         MSG_PRINTF(LOG_ERR, "file_name:%s, verify signature failed!!\n", file_name);
         goto end;
     }
@@ -156,16 +154,19 @@ end:
     return ret;
 }
 
-rt_bool inspect_abstract_content(uint8_t *hash, uint8_t *signature)
+rt_bool inspect_abstract_content(uint8_t *input, uint8_t *signature)
 {
     rt_bool ret = RT_FALSE;
 
-    uint8_t pk_base64_out[MAX_PK_LEN/2 + 1] = {0};
+    uint8_t pk_out[MAX_PK_LEN/2 + 1] = {0};
     uint16_t p_len = 0;
 
-    hexstring2bytes(g_verify_pk, pk_base64_out, &p_len);
-
-    ret = ecc_verify(hash, MAX_FILE_HASH_LEN/2, pk_base64_out, MAX_PK_LEN/2, signature, PRIVATE_HASH_STR_LEN/2);
+    hexstring2bytes(g_verify_pk, pk_out, &p_len);
+    // MSG_INFO_ARRAY("input: ", input, MAX_FILE_HASH_LEN/2);
+    MSG_PRINTF(LOG_INFO, "input: %s\n", input);
+    MSG_INFO_ARRAY("signature: ", signature, PRIVATE_HASH_STR_LEN/2);
+    MSG_INFO_ARRAY("pk: ", pk_out, p_len);
+    ret = ecc_verify(input, MAX_FILE_HASH_LEN, pk_out, p_len, signature, PRIVATE_HASH_STR_LEN/2);
 
     return ret;
 }
