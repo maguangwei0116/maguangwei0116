f00c0e04277479f1aabde19890990f6bef030435
pyx1104-hash <yuxiang.peng@redteamobile.com>
2020-06-19 09:40:53 +0800

    fix:Fix code style

--- sysapp/agent/core/config/inc/config.h
+++ sysapp/agent/core/config/inc/config.h
@@ -11,13 +11,19 @@
 
 #include "stdint.h"
 
+typedef enum SIM_MODE_TYPE {
+    SIM_MODE_TYPE_VUICC_ONLY = 0,     // vUICC mode
+    SIM_MODE_TYPE_SIM_FIRST,
+    SIM_MODE_TYPE_SIM_ONLY
+} sim_mode_type_e;
+
 typedef struct CONFIG_INFO {
     char *              oti_addr;           // OTI server addr
     char *              emq_addr;           // EMQ server addr
     char *              proxy_addr;         // proxy server addr
     uint8_t             lpa_channel_type;   // @ref lpa_channel_type_e
 #ifdef CFG_REDTEA_READY_ON
-    uint8_t             sim_mode;           // sim mode
+    sim_mode_type_e     sim_mode;           // sim mode
 #endif
     uint32_t            log_max_size;       // unit: MB
     uint8_t             monitor_log_level;  // @ref log_level_e in log.h
@@ -29,12 +35,6 @@ typedef struct CONFIG_INFO {
     /* some config item which never changed dynamically */
     uint32_t            oti_port;           // port for OTI server
 } config_info_t;
-
-typedef enum SIM_MODE_TYPE {
-    SIM_MODE_TYPE_VUICC_ONLY = 0,     // vUICC mode
-    SIM_MODE_TYPE_SIM_FIRST,
-    SIM_MODE_TYPE_SIM_ONLY
-} sim_mode_type_e;
 
 int32_t init_config(void *arg);
 
