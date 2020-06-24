ebf874bc5a95d683df6f6239d8fc4be036220b6e
pyx1104-hash <yuxiang.peng@redteamobile.com>
2020-06-24 11:02:21 +0800

    add:Ping returns success when an external message is received

--- library/ping/src/ping.c
+++ library/ping/src/ping.c
@@ -9,40 +9,42 @@
 #include <string.h>
 #include <sys/time.h>
 #include <errno.h>
-
 #include "rt_type.h"
 
-#define DATA_SIZE 32
+#define RT_DATA_SIZE        32
+#define RT_PING_TIMES       10
+
+rt_bool  g_downstream_event = RT_FALSE;
 
 typedef struct tag_icmp_header
 {
-    u_int8_t  type;
-    u_int8_t  code;
-    u_int16_t check_sum;
-    u_int16_t id;
-    u_int16_t seq;
+    uint8_t         type;
+    uint8_t         code;
+    uint16_t        check_sum;
+    uint16_t        id;
+    uint16_t        seq;
 } icmp_header;
 
 typedef struct tag_iphdr
 {
-    u_int8_t        ip_head_verlen;
-    u_int8_t        ip_tos;
-    unsigned short  ip_length;
-    unsigned short  ip_id;
-    unsigned short  ip_flags;
-    u_int8_t        ip_ttl;
-    u_int8_t        ip_protacol;
-    unsigned short  ip_checksum;
-    int             ip_source;
-    int             ip_destination;
+    uint8_t         ip_head_verlen;
+    uint8_t         ip_tos;
+    uint16_t        ip_length;
+    uint16_t        ip_id;
+    uint16_t        ip_flags;
+    uint8_t         ip_ttl;
+    uint8_t         ip_protacol;
+    uint16_t        ip_checksum;
+    int32_t         ip_source;
+    int32_t         ip_destination;
 } ip_header;
 
-unsigned short generation_checksum(unsigned short * buf, int size)
+uint16_t generation_checksum(uint16_t *buf, int32_t size)
 {
     unsigned long cksum = 0;
     while(size > 1) {
         cksum += *buf++;
-        size -= sizeof(unsigned short);
+        size -= sizeof(uint16_t);
     }
 
     if(size) {
@@ -52,10 +54,10 @@ unsigned short generation_checksum(unsigned short * buf, int size)
     cksum =  (cksum>>16) + (cksum & 0xffff);
     cksum += (cksum>>16);
 
-    return (unsigned short)(~cksum);
+    return (uint16_t)(~cksum);
 }
 
-double get_time_interval(struct timeval * start, struct timeval * end)
+double get_time_interval(struct timeval *start, struct timeval *end)
 {
     double interval;
     struct timeval tp;
@@ -72,18 +74,18 @@ double get_time_interval(struct timeval * start, struct timeval * end)
     return interval;
 }
 
-int ping_host_ip(const char * domain, double *avg_delay, int *lost, double *mdev)
+int32_t ping_host_ip(const uint8_t *domain, double *avg_delay, int32_t *lost, double *mdev)
 {
-    int i;
-    int ret = -1;
-    int client_fd;
-    int size = 50 * 1024;
-    int recv_count = 0;
+    int32_t i;
+    int32_t ret = RT_ERROR;
+    int32_t client_fd;
+    int32_t size = 50 * 1024;
+    int32_t recv_count = 0;
     double time_sum = 0;
-    double time_interval[10] = {0};
+    double time_interval[RT_PING_TIMES + 1] = {0};
     double time_mdev = 0;
     struct timeval timeout;
-    char * icmp;
+    uint8_t * icmp;
     in_addr_t dest_ip;
     icmp_header * icmp_head;
     struct sockaddr_in dest_socket_addr;
@@ -103,7 +105,7 @@ int ping_host_ip(const char * domain, double *avg_delay, int *lost, double *mdev
     }
 
     client_fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-    if (client_fd == -1) {
+    if (client_fd == RT_ERROR) {
         MSG_PRINTF(LOG_ERR, "socket error: %s !\n", strerror(errno));
         return ret;
     }
@@ -127,8 +129,8 @@ int ping_host_ip(const char * domain, double *avg_delay, int *lost, double *mdev
     dest_socket_addr.sin_port = htons(0);
     memset(dest_socket_addr.sin_zero, 0, sizeof(dest_socket_addr.sin_zero));
 
-    icmp = (char *)malloc(sizeof(icmp_header) + DATA_SIZE);
-    memset(icmp, 0, sizeof(icmp_header) + DATA_SIZE);
+    icmp = (uint8_t *)malloc(sizeof(icmp_header) + RT_DATA_SIZE);
+    memset(icmp, 0, sizeof(icmp_header) + RT_DATA_SIZE);
 
     icmp_head = (icmp_header *)icmp;
     icmp_head->type = 8;
@@ -137,31 +139,36 @@ int ping_host_ip(const char * domain, double *avg_delay, int *lost, double *mdev
 
     // MSG_PRINTF(LOG_DBG, "PING %s (%s)\n", domain, inet_ntoa(*((struct in_addr*)&dest_ip)));
 
-    for(i = 0; i < 10; i++) {
-        sleep(1);
+    for(i = 0; i < RT_PING_TIMES; i++) {
         struct timeval start;
         struct timeval end;
         long result;
         struct sockaddr_in from;
         socklen_t from_packet_len;
         long read_length;
-        char recv_buf[1024];
+        uint8_t recv_buf[1024];
+
+        if (g_downstream_event == RT_TRUE) {
+            MSG_PRINTF(LOG_INFO, "External events interrupt ping ! Hold using card...\n");
+            g_downstream_event = RT_FALSE;
+            return RT_SUCCESS;
+        }
+        sleep(1);
 
         icmp_head->seq = htons(i);
         icmp_head->check_sum = 0;
-        icmp_head->check_sum = generation_checksum((unsigned short*)icmp,
-            sizeof(icmp_header) + DATA_SIZE);
+        icmp_head->check_sum = generation_checksum((uint16_t*)icmp,
+            sizeof(icmp_header) + RT_DATA_SIZE);
         gettimeofday(&start, NULL);
         result = sendto(client_fd, icmp, sizeof(icmp_header) +
-            DATA_SIZE, 0, (struct sockaddr *)&dest_socket_addr,
+            RT_DATA_SIZE, 0, (struct sockaddr *)&dest_socket_addr,
             sizeof(dest_socket_addr));
 
-        if(result == -1) {
+        if(result == RT_ERROR) {
             time_sum += 9999.99;
             time_interval[i] = 9999.99;
             MSG_PRINTF(LOG_INFO, "time_sum is %lf\n", time_sum);
-            MSG_PRINTF(LOG_DBG, "PING: sendto: Network is unreachable\n");
-
+            MSG_PRINTF(LOG_ERR, "PING: sendto: Network is unreachable\n");
             continue;
         }
 
@@ -169,15 +176,13 @@ int ping_host_ip(const char * domain, double *avg_delay, int *lost, double *mdev
         memset(recv_buf, 0, sizeof(recv_buf));
 
         while(1) {
-            read_length = recvfrom(client_fd, recv_buf, 1024, 0,
-                (struct sockaddr*)&from, &from_packet_len);
+            read_length = recvfrom(client_fd, recv_buf, 1024, 0, (struct sockaddr*)&from, &from_packet_len);
             gettimeofday( &end, NULL );
 
-            if(read_length != -1) {
+            if(read_length != RT_ERROR) {
                 ip_header * recv_ip_header = (ip_header*)recv_buf;
-                int ip_ttl = (int)recv_ip_header->ip_ttl;
-                icmp_header * recv_icmp_header = (icmp_header *)(recv_buf +
-                    (recv_ip_header->ip_head_verlen & 0x0F) * 4);
+                int32_t ip_ttl = (int32_t)recv_ip_header->ip_ttl;
+                icmp_header * recv_icmp_header = (icmp_header *)(recv_buf + (recv_ip_header->ip_head_verlen & 0x0F) * 4);
 
                 if(recv_icmp_header->type != 0) {
                     MSG_PRINTF(LOG_ERR, "error type %d received, error code %d \n", recv_icmp_header->type, recv_icmp_header->code);
@@ -189,20 +194,19 @@ int ping_host_ip(const char * domain, double *avg_delay, int *lost, double *mdev
                     continue;
                 }
 
-                if(read_length >= (sizeof(ip_header) + sizeof(icmp_header) + DATA_SIZE)) {
+                if(read_length >= (sizeof(ip_header) + sizeof(icmp_header) + RT_DATA_SIZE)) {
                     double tmp_time = get_time_interval(&start, &end);
 
                     // MSG_PRINTF(LOG_DBG, "%ld bytes from %s (%s): icmp_seq=%d ttl=%d time=%.2f ms\n",
                     //     read_length, domain, inet_ntoa(from.sin_addr), recv_icmp_header->seq / 256,
                     //     ip_ttl, tmp_time);
 
-                    ret = 0;
                     recv_count ++;
                     time_sum += tmp_time;
                     time_interval[i] = tmp_time;
 
                     // MSG_PRINTF(LOG_INFO, "time_sum is %lf\n", time_sum);
-                    // MSG_PRINTF(LOG_INFO, "time_interval[%d] is %lf\n", i, time_interval[i]);
+                    MSG_PRINTF(LOG_INFO, "time_interval[%d] is %lf\n", i, time_interval[i]);
                 }
 
                 break;
@@ -221,35 +225,35 @@ PING_EXIT:
         icmp = NULL;
     }
 
-    if(client_fd != -1) {
+    if(client_fd != RT_ERROR) {
         close(client_fd);
     }
 
-    for (i = 0; i < 10; ++i) {
-        if (time_sum / 10 > time_interval[i]) {
-            time_mdev += time_sum / 10 - time_interval[i];
+    for (i = 0; i < RT_PING_TIMES; ++i) {
+        if (time_sum/RT_PING_TIMES > time_interval[i]) {
+            time_mdev += time_sum/RT_PING_TIMES - time_interval[i];
         } else {
-            time_mdev += time_interval[i] - time_sum / 10;
+            time_mdev += time_interval[i] - time_sum/RT_PING_TIMES;
         }
     }
 
-    // MSG_PRINTF(LOG_INFO, "lost is %d\n", (10 - recv_count) * 10);
-    // MSG_PRINTF(LOG_INFO, "time_sum / 10 is %lf\n", time_sum / 10);
-    // MSG_PRINTF(LOG_INFO, "time_mdev / 10 is %lf\n", time_mdev / 10);
+    // MSG_PRINTF(LOG_INFO, "lost  : %d\n", (RT_PING_TIMES - recv_count)*10);
+    // MSG_PRINTF(LOG_INFO, "dekay : %lf\n", time_sum/RT_PING_TIMES);
+    // MSG_PRINTF(LOG_INFO, "mdev  : %lf\n", time_mdev/RT_PING_TIMES);
 
-    *lost = (10 - recv_count) * 10;
-    *avg_delay = time_sum / 10;
-    *mdev = time_mdev / 10;
+    *lost = RT_PING_TIMES - recv_count;
+    *avg_delay = time_sum/RT_PING_TIMES;
+    *mdev = time_mdev/RT_PING_TIMES;
 
     return ret;
 }
 
-int local_ping(int argc, char *argv[], double *avg_delay, int *lost, double *mdev)
+int32_t local_ping(int32_t argc, uint8_t *argv[], double *avg_delay, int32_t *lost, double *mdev)
 {
     ping_host_ip(argv[3], avg_delay, lost, mdev);
 }
 
-int rt_local_ping(char *ip, double *delay, int *lost, double *mdev)
+int32_t rt_local_ping(uint8_t *ip, double *delay, int32_t *lost, double *mdev)
 {
     ping_host_ip(ip, delay, lost, mdev);
 }
