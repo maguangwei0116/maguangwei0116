bf5b7f71a0753262d109876806f92ef6e49ba92b
pyx1104-hash <yuxiang.peng@redteamobile.com>
2020-07-09 16:24:33 +0800

    fix:The domain name of the default monitoring policy is changed to IP

--- sysapp/agent/core/usrdata/src/usrdata.c
+++ sysapp/agent/core/usrdata/src/usrdata.c
@@ -15,7 +15,7 @@
 #include "stdint.h"
 #include "rt_type.h"
 
-#define DEFAULT_STRATEGY "{\"enabled\":true,\"interval\":10,\"type\":1,\"strategies\":[{\"domain\":\"18.136.190.97\",\"level\":1}]}"
+#define DEFAULT_STRATEGY "{\"enabled\":true,\"interval\":10,\"type\":1,\"strategies\":[{\"domain\":\"cthulhu.easyiot.ai\",\"level\":1}]}"
 
 static rt_bool network_strategy_check_memory(const void *buf, int32_t len, int32_t value)
 {
