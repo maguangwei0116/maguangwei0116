ca7f0a8b5823d2d9049b29ab4772b78814378562
pyx1104-hash <yuxiang.peng@redteamobile.com>
2020-09-29 15:17:42 +0800

    fix:Modify auto connect and iptables

--- sysapp/agent/core/config/src/mbn.c
+++ sysapp/agent/core/config/src/mbn.c
@@ -9,12 +9,15 @@
 #include "log.h"
 #include "agent_queue.h"
 
+#define AT_ROAMSERVICE              "AT+QCFG=\"ROAMSERVICE\",2\r\n"     // 2 is force open
+#define AT_AUTO_CONNECT             "AT+QCFG=\"QCAUTOCONNECT\",0\r\n"
+#define AT_ECM                      "AT+QCFG=\"USBNET\",1\r\n"
+
 #define MBN_USED_ITEM               "at+qmbncfg=\"select\"\r\n"
 #define MBN_CONFIG_ONE_ITEM         "at+qmbncfg=\"select\",\"%s\"\r\n"
 #define MBN_AUTO_SELECT_STATE       "at+qmbncfg=\"autosel\"\r\n"
 #define MBN_CONFIG_AUTO_STATE       "at+qmbncfg=\"autosel\",%d\r\n"
 #define MNB_CONFIG_DEACTUCATE       "at+qmbncfg=\"Deactivate\"\r\n"
-#define AT_ROAMSERVICE              "AT+QCFG=\"ROAMSERVICE\",2\r\n"   // 2 is force open
 #define MBN_ROW_ITEM                "ROW_Generic_3GPP"
 #define MBN_AUTO_SELECT             "AutoSel"
 #define MBN_ECHO_OFF                "ATE0\r\n"
@@ -90,12 +93,12 @@ static rt_bool mbn_set_auto_state(int32_t state)
     if (at_send_recv(MNB_CONFIG_DEACTUCATE, at_rsp, sizeof(at_rsp), MBN_AT_TIMEOUT) != RT_SUCCESS) {
         MSG_PRINTF(LOG_WARN, "send data error\n");
     }
-    
+
     snprintf(at_req, sizeof(at_req), MBN_CONFIG_AUTO_STATE, state);
     if (at_send_recv(at_req, at_rsp, sizeof(at_rsp), MBN_AT_TIMEOUT) != RT_SUCCESS) {
         MSG_PRINTF(LOG_WARN, "send data error\n");
     }
-    
+
     return RT_TRUE;
 }
 
@@ -104,6 +107,8 @@ static int32_t mbn_config_device(void)
     char at_rsp[128];
     
     at_send_recv(AT_ROAMSERVICE, at_rsp, sizeof(at_rsp), MBN_AT_TIMEOUT);       // Set roaming switch
+    at_send_recv(AT_AUTO_CONNECT, at_rsp, sizeof(at_rsp), MBN_AT_TIMEOUT);      // Cancel automatic dialing
+    at_send_recv(AT_ECM, at_rsp, sizeof(at_rsp), MBN_AT_TIMEOUT);               // Set ECM
     at_send_recv(MBN_ECHO_OFF, at_rsp, sizeof(at_rsp), MBN_AT_TIMEOUT);
     
     if (mbn_get_auto_state() == RT_FALSE) {
@@ -113,7 +118,7 @@ static int32_t mbn_config_device(void)
             return RT_SUCCESS;
         }
     }
-    
+
     mbn_set_auto_state(0);
     mbn_set_item(MBN_ROW_ITEM);
     MSG_PRINTF(LOG_WARN, "Reboot to active MBN config ...\r\n");
