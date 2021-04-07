547bff35d0aa5f1c273c615aa483ac6347d78d1a
LiXinRTM <54013463+lixin-redteamobile@users.noreply.github.com>
2021-04-07 12:05:28 +0800

    Feat: Add DETECT and ON_DETECT

--- sysapp/agent/core/networkd/src/upload_network.c
+++ sysapp/agent/core/networkd/src/upload_network.c
@@ -17,7 +17,7 @@
 #include "cJSON.h"
 #include "md5.h"
 
-static cJSON *upload_on_network_packer(void *arg)
+static cJSON *upload_on_detect_packer(void *arg)
 {
     MSG_PRINTF(LOG_INFO, "Upload on network\n");
     
@@ -25,7 +25,7 @@ exit_entry:
     return (cJSON *)arg;
 }
 
-static int32_t network_parser(const void *in, char *tranid, void **out)
+static int32_t detect_parser(const void *in, char *tranid, void **out)
 {
     int32_t ret = RT_ERROR;
     cJSON *agent_msg = NULL;
@@ -82,31 +82,132 @@ static int32_t network_parser(const void *in, char *tranid, void **out)
     return ret;
 }
 
-static int32_t network_handler(const void *in, const char *event, void **out)
+static int32_t exec_ping_cmd(const char* domain, int32_t times, uint8_t* out, int32_t out_size)
 {
-    uint8_t buf[1024];
-    const int8_t *cmd = "ping 23.91.101.68 -W 10 -c 5";
+    int32_t out_len = 0;
+    int8_t cmd_buf[256];
+    int8_t temp[4];
+    rt_os_memset(cmd_buf, 0, sizeof(cmd_buf));
+    rt_os_memset(temp, 0, sizeof(temp));
+    rt_os_strcat(cmd_buf, "ping ");
+    rt_os_strcat(cmd_buf, domain);
+    rt_os_strcat(cmd_buf, " -c ");
+    snprintf(temp, sizeof(temp), "%d", times),
+    rt_os_strcat(cmd_buf, temp);
+    out_len = shell_cmd(cmd_buf, out, out_size);
+    out[out_len++] = '\0';
+    return out_len;
+}
+
+static int32_t detect_handler(const void *in, const char *event, void **out)
+{
     int32_t num;
+    int32_t all_domains_num = 0;
+    int32_t ii = 0;
     int32_t state = RT_SUCCESS;
+    uint8_t buf[2048];
+    int32_t out_len = 0;
+    double  packet_loss = 0;
+    double  min_latency = 0;
+    double  avg_latency = 0;
+    double  max_latency = 0;
     cJSON *payload = NULL;
     cJSON *content = NULL;
+    cJSON *ping = NULL;
+    cJSON *times = NULL;
+    cJSON *domains = NULL;
+    cJSON *domain = NULL;
+    cJSON *content_r = NULL;
+    cJSON *ping_info = NULL;
+    cJSON *pong = NULL;
+    char  *ptr = NULL;
+    const char* PACK_LOSS_HEAD = "received, ";
+    const char* RTT_HEAD = "min/avg/max = ";
 
-    num = shell_cmd(cmd, buf, sizeof(buf));
-    buf[num] = '\0';
-    MSG_PRINTF(LOG_TRACE, "num:%d, buf:%s\n", num, buf);
-
-    content = cJSON_CreateObject();
-    if (!content) {
-        MSG_PRINTF(LOG_ERR, "content buffer is empty\n");
+    content_r = cJSON_CreateObject();
+    if (!content_r) {
+        MSG_PRINTF(LOG_ERR, "content_r buffer is empty\n");
         goto end;
     }
-    cJSON_AddItemToObject(content, "network_info", cJSON_CreateString(buf));
-    *out = content;
+    pong = cJSON_CreateArray();
+    if (!pong) {
+        MSG_PRINTF(LOG_ERR, "pong buffer is empty\n");
+        goto end;
+    }
+    do {
+        payload = cJSON_Parse((uint8_t *)in);
+        if (!payload) {
+            MSG_PRINTF(LOG_ERR, "Parse payload failed!!\n");
+            break;
+        }
+        content = cJSON_GetObjectItem(payload, "content");
+        if (!content) {
+            MSG_PRINTF(LOG_ERR, "Parse content failed!!\n");
+            break;
+        }
+        ping = cJSON_GetObjectItem(content, "ping");
+        if (!ping) {
+            MSG_PRINTF(LOG_ERR, "Parse ping failed!!\n");
+            break;
+        }
+        times = cJSON_GetObjectItem(ping, "times");
+        if (!times) {
+            MSG_PRINTF(LOG_ERR, "Parse times failed!!\n");
+            break;
+        }
+        num = times->valueint;
+        domains = cJSON_GetObjectItem(ping, "domains");
+        if (!domains) {
+            MSG_PRINTF(LOG_ERR, "Parse domains failed!!\n");
+            break;
+        }
+        all_domains_num = cJSON_GetArraySize(domains);
+        for (ii = 0; ii < all_domains_num; ii++) {
+            domain = cJSON_GetArrayItem(domains, ii);
+            if (!domain) {
+                MSG_PRINTF(LOG_ERR, "Parse domain failed!!\n");
+                continue;
+            }
+            ping_info = cJSON_CreateObject();
+            if (!ping_info) {
+                MSG_PRINTF(LOG_ERR, "Ping info create error\n");
+                continue;
+            }
+            rt_os_memset(buf, 0, sizeof(buf));
+            out_len = exec_ping_cmd(domain->valuestring, num, buf, sizeof(buf));
+            ptr = rt_os_strstr(buf, PACK_LOSS_HEAD);
+            if (!ptr) {
+                packet_loss = 100;
+            } else {
+                packet_loss = strtod(ptr + strlen(PACK_LOSS_HEAD), &ptr);
+            }
+            ptr = strstr(buf, RTT_HEAD);
+            if (!ptr) {
+                min_latency = 0;
+                avg_latency = 0;
+                max_latency = 0;
+            } else {
+                min_latency = strtod(ptr + strlen(RTT_HEAD), &ptr);
+                avg_latency = strtod(ptr + 1, &ptr);
+                max_latency = strtod(ptr + 1, &ptr);                
+            }
+            MSG_PRINTF(LOG_TRACE, "packet_loss:%f, min:%f, avg:%f, max:%f \n", packet_loss, min_latency, avg_latency, max_latency);
+            cJSON_AddItemToObject(ping_info, "domain", cJSON_CreateString(domain->valuestring));
+            cJSON_AddItemToObject(ping_info, "result", cJSON_CreateString(buf));            
+            cJSON_AddItemToObject(ping_info, "packetLoss", cJSON_CreateNumber(packet_loss));
+            cJSON_AddItemToObject(ping_info, "maxLatency", cJSON_CreateNumber(max_latency));
+            cJSON_AddItemToObject(ping_info, "minLatency", cJSON_CreateNumber(min_latency));
+            cJSON_AddItemToObject(ping_info, "avgLatency", cJSON_CreateNumber(avg_latency));
+            cJSON_AddItemToArray(pong, ping_info);
+        }
+        cJSON_AddItemToObject(content_r, "pong", pong);
+    } while(0);
+    *out = content_r;
 end:
     rt_os_free((void *)in);
     return state;
 }
 
-DOWNSTREAM_METHOD_OBJ_INIT(NETWORK, MSG_ID_IDLE, ON_NETWORK, network_parser, network_handler);
-UPLOAD_EVENT_OBJ_INIT(ON_NETWORK, TOPIC_DEVICEID_OR_EID, upload_on_network_packer);
+DOWNSTREAM_METHOD_OBJ_INIT(DETECT, MSG_ID_IDLE, ON_DETECT, detect_parser, detect_handler);
+UPLOAD_EVENT_OBJ_INIT(ON_DETECT, TOPIC_DEVICEID_OR_EID, upload_on_detect_packer);
 
